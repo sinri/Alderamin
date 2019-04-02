6ec50a51663992fc481a79784eff8336005e490e
Sinri Edogawa <e.joshua.s.e@gmail.com>
2019-04-02 15:06:58 +0800

    Redesign Bootstrap Sample

--- demo/test/bootstrap.php
+++ demo/test/bootstrap.php
@@ -13,14 +13,8 @@ date_default_timezone_set("Asia/Shanghai");
 
 $alderaminConfig = new \sinri\Alderamin\core\configuration\AlderaminConfig();
 
-$alderaminConfig->setCorePdoConfig(new \sinri\ark\database\pdo\ArkPDOConfig([
-    \sinri\ark\database\pdo\ArkPDOConfig::CONFIG_TITLE => "ALDERAMIN",
-    \sinri\ark\database\pdo\ArkPDOConfig::CONFIG_HOST => "127.0.0.1",
-    \sinri\ark\database\pdo\ArkPDOConfig::CONFIG_PORT => 3306,
-    \sinri\ark\database\pdo\ArkPDOConfig::CONFIG_USERNAME => "sinri",
-    \sinri\ark\database\pdo\ArkPDOConfig::CONFIG_PASSWORD => "123456",
-    \sinri\ark\database\pdo\ArkPDOConfig::CONFIG_DATABASE => "alderamin",
-]));
+require_once __DIR__ . '/config/pdo.php';
+$alderaminConfig->setCorePdoConfig($pdoConfig);
 $alderaminConfig->setCraftStore(__DIR__ . '/../store/craft-store');
 $alderaminConfig->setReportStore(__DIR__ . '/../store/report-store');
 $alderaminConfig->setUnitStoreNamespace("sinri\\Alderamin\\test\\units");
@@ -29,21 +23,7 @@ $alderaminConfig->setCronMax(5);
 $alderaminConfig->setLogBaseLevel(\Psr\Log\LogLevel::DEBUG);
 $alderaminConfig->setLogDirPath(__DIR__ . '/../log');
 
-
-$readNodeConfig = (new \sinri\ark\database\mysqli\ArkMySQLiConfig())
-    ->setTitle("ALDERAMIN_READ")
-    ->setHost("127.0.0.1")
-    ->setPort(3306)
-    ->setUsername("sinri")
-    ->setPassword("123456")
-    ->setDatabase("test");
-$writeNodeConfig = (new \sinri\ark\database\mysqli\ArkMySQLiConfig())
-    ->setTitle("ALDERAMIN_READ")
-    ->setHost("127.0.0.1")
-    ->setPort(3306)
-    ->setUsername("sinri")
-    ->setPassword("123456")
-    ->setDatabase("test");
+require_once __DIR__ . '/config/nodes.php';
 
 $alderaminConfig->setNodesMySQLiConfigList([
     \sinri\Alderamin\core\configuration\AlderaminConfig::KEY_READ_NODE => $readNodeConfig,
